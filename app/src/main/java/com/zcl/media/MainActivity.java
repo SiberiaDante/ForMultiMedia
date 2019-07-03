31fa496254374b792ecacff4eb4254cdc3f19c8a
SiberiaDante <994537867@qq.com>
2019-07-03 15:39:59 +0800

    create project

---
+++ app/src/main/java/com/zcl/media/MainActivity.java
@@ -0,0 +1,49 @@
+package com.zcl.media;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.zcl.media.activity.BitmapActivity;
+
+public class MainActivity extends AppCompatActivity {
+    String[] mPermissionList = new String[]{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE};
+    public static final int REQUEST_PICK_IMAGE = 11101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(MainActivity.this, mPermissionList, 100);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 100:
+                boolean writeExternalStorage = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean readExternalStorage = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                if (grantResults.length > 0 && writeExternalStorage && readExternalStorage) {
+                    jumpToActivity(BitmapActivity.class);
+                } else {
+                    Toast.makeText(this, "请设置必要权限", Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+        }
+    }
+
+    private void jumpToActivity(Class cls) {
+        startActivity(new Intent(this, cls));
+    }
+}
